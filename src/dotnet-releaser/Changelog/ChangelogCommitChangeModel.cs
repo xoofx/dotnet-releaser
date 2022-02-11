f0c63395317c76e736e5f6018a3f582cbf1ba512
Alexandre Mutel <alexandre_mutel@live.com>
2022-02-11 09:43:59 +0100

    Make sha default to 8 chars instead of full_sha

--- src/dotnet-releaser/Changelog/ChangelogCommitChangeModel.cs
+++ src/dotnet-releaser/Changelog/ChangelogCommitChangeModel.cs
@@ -1,5 +1,10 @@
-﻿namespace DotNetReleaser.Changelog;
+﻿using System;
+
+namespace DotNetReleaser.Changelog;
 
 public record ChangelogChangeModel(string Title, string Body, string Author);
 
-public record ChangelogCommitChangeModel(string Title, string Body, string Author, string Sha) : ChangelogChangeModel(Title, Body, Author);
+public record ChangelogCommitChangeModel(string Title, string Body, string Author, string FullSha) : ChangelogChangeModel(Title, Body, Author)
+{
+    public string Sha => FullSha.Substring(0, Math.Min(FullSha.Length, 8));
+}
