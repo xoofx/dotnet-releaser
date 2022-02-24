3ac6f27710db4b5bafd69c8df241bf94e1c0e08a
Alexandre Mutel <alexandre_mutel@live.com>
2022-02-24 06:34:52 +0100

    Add support for authorized release branches

--- src/dotnet-releaser/Configuration/DevHostingConfiguration.cs
+++ src/dotnet-releaser/Configuration/DevHostingConfiguration.cs
@@ -1,4 +1,6 @@
-﻿namespace DotNetReleaser.Configuration;
+﻿using System.Collections.Generic;
+
+namespace DotNetReleaser.Configuration;
 
 /// <summary>
 /// Shared configuration for dev hosting.
@@ -12,6 +14,7 @@ public abstract class DevHostingConfiguration : ConfigurationBase
         Api = string.Empty;
         User = string.Empty;
         Repo = string.Empty;
+        Branches = new List<string>();
     }
 
     internal abstract string Provider { get; }
@@ -26,7 +29,18 @@ public abstract class DevHostingConfiguration : ConfigurationBase
 
     public string VersionPrefix { get; set; }
 
+    public List<string> Branches { get; }
+
     public string GetUrl() => $"{Base.Trim('/')}/{User}/{Repo}";
+
+    public void AddDefaults()
+    {
+        // Don't add default branches if they are added manually
+        if (Branches.Count > 0) return;
+
+        Branches.Add("main");
+        Branches.Add("master");
+    }
 
     public override string ToString()
     {
